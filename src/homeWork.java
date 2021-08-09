ea921f9f5251931eb6ce3b39fe03259ef6c66595
x-Beo-x <86937244+x-Beo-x@users.noreply.github.com>
2021-08-09 14:12:46 +0700

    fixed

--- src/homeWork.java
+++ src/homeWork.java
@@ -1,16 +1,34 @@
 import java.util.Scanner;
 
-public class homeWord {
-    public static String[] list = {"iphone6", "iphone7", "iphone8"};
+public class homeWork {
+
+    public static String[] list;
     public static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
         boolean flag = true;
+        list = inputListElement(list);
         while (flag) {
             showList();
             nextActinies();
             showActivitiesMenu();
         }
+    }
+
+    public static String[] inputListElement(String[] arr){
+        System.out.println("Input list Size");
+        int size = sc.nextInt();
+        while(size < 0 ){
+            System.out.println("please input valid size (size>0)");
+            sc.nextInt();
+        }
+        arr = new String[size];
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Input elment at " + i + " position");
+            arr[i] = sc.next();
+        }
+        System.out.println("Input");
+        return arr;
     }
 
     public static void showList() {
@@ -45,49 +63,70 @@ public class homeWord {
     public static void operating(int option) {
         switch (option) {
             case 1:
-                addNewProduct();
+                System.out.println("Input your new product name");
+                String productName = sc.next();
+
+                list = addNewProduct(productName);
                 break;
+
             case 2:
-                removeProduct();
+                System.out.println("Input productId");
+                int productId = sc.nextInt();
+
+                while (productId < 0 || productId > list.length - 1) {
+                    System.out.println("can not find product with the same ID, please input again");
+                    productId = sc.nextInt();
+                }
+
+                list = removeProduct(productId);
                 break;
+
             case 3:
-                findProduct();
+                System.out.println("Input your wanted product");
+                String targetProduct = sc.next();
+
+                System.out.println(findProduct(targetProduct));
                 break;
+
             case 4:
-                editProduct();
+                System.out.println("Input product Id");
+                int index = sc.nextInt();
+
+                while (index < 0 || index > list.length - 1) {
+                    System.out.println("productId is out of range, please input again");
+                    index = sc.nextInt();
+                }
+
+                System.out.println("Input product name");
+                String newName = sc.next();
+
+                list = editProduct(index, newName, list);
                 break;
         }
     }
 
-    public static String[] addNewProduct() {
-        sc.nextLine();
-        System.out.println("Input your new product name");
-        String productName = sc.nextLine();
-
-        String[] temp = new String[list.length + 1];
-        temp[temp.length - 1] = productName;
-
-        System.arraycopy(list, 0, temp, 0, list.length);
+    public static String[] addNewProduct(String productName) {
+        String[] temp = list;
 
         if (confirm()) {
-            list = new String[temp.length];
-            System.arraycopy(temp, 0, list, 0, temp.length);
+            temp = new String[list.length + 1];
+            temp[temp.length - 1] = productName;
+            System.arraycopy(list, 0, temp, 0, list.length);
             System.out.println("Add new product successfully");
         } else {
             System.out.println("Add new product canceled");
         }
-
-        return list;
+        return temp;
     }
 
-    public static String[] removeProduct() {
-        System.out.println("Input productId");
-        int productId = sc.nextInt();
+    public static String[] removeProduct(int productId) {
+//        System.out.println("Input productId");
+//        int productId = sc.nextInt();
 
-        while (productId < 0 || productId > list.length - 1) {
-            System.out.println("can not find product with the same ID");
-            productId = sc.nextInt();
-        }
+//        while (productId < 0 || productId > list.length - 1) {
+//            System.out.println("can not find product with the same ID");
+//            productId = sc.nextInt();
+//        }
 
         String[] temp = new String[list.length - 1];
         for (int i = 0; i < temp.length; i++) {
@@ -97,51 +136,52 @@ public class homeWord {
                 temp[i] = list[i + 1];
             }
         }
+
         if (confirm()) {
             list = new String[temp.length];
             System.arraycopy(temp, 0, list, 0, temp.length);
         }
+
         return list;
     }
 
-    public static void findProduct() {
-        System.out.println("Input your wanted product");
-
-        String targetProduct = sc.next();
-        boolean isFound = false;
-        String result = "";
+    public static String findProduct(String targetProduct) {
+//        System.out.println("Input your wanted product");
+//
+//        String targetProduct = sc.next();
+        String display = "";
+        int index = -1;
         for (int i = 0; i < list.length; i++) {
             if (list[i].equals(targetProduct)) {
-                isFound = true;
+
+                index = i;
                 break;
             }
         }
-        if (isFound) {
-            System.out.println("We found your product in the list");
+        if (index != -1) {
+            display = "We found your product in the list at index: " + index;
         } else {
-            System.out.println("we can not foudn your product in the list");
+            display = "we can not foudn your product in the list";
         }
+
+        return display;
     }
 
-    public static void editProduct() {
-        System.out.println("Input product Id");
-        int index = sc.nextInt();
-        while (index < 0 || index > list.length - 1) {
-            System.out.println("productId is out of range, please input again");
-            index = sc.nextInt();
-        }
-        System.out.println("Input product name");
-        String newName = sc.next();
+    public static String[] editProduct(int index, String newName, String[] arr) {
+        arr = list;
         if (confirm()) {
-            list[index] = newName;
+            arr[index] = newName;
         }
-
+        return arr;
     }
 
     public static boolean confirm() {
         System.out.println("-----------------------------");
         System.out.println("Do you want to continue? (press any key to continue or press N/n to deny)");
+
         boolean isContinue;
+        sc.nextLine();
+
         char confirm = sc.next().charAt(0);
         switch (confirm) {
             default:
